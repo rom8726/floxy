3afd06cb5d9c25e4eb16e493cdfdd61333131cc1
Roman Chudov <roman.chudov@gmail.com>
2025-10-22 06:50:00 +0300

    executionContext implemented.

---
+++ execution_context.go
@@ -0,0 +1,38 @@
+package floxy
+
+import (
+	"sync"
+)
+
+type executionContext struct {
+	instanceID int64
+	stepName   string
+	retryCount int
+	variables  map[string]any
+	mu         sync.RWMutex
+}
+
+func (c *executionContext) InstanceID() int64 {
+	return c.instanceID
+}
+
+func (c *executionContext) StepName() string {
+	return c.stepName
+}
+
+func (c *executionContext) RetryCount() int {
+	return c.retryCount
+}
+
+func (c *executionContext) GetVariable(key string) (any, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	val, ok := c.variables[key]
+	return val, ok
+}
+
+func (c *executionContext) SetVariable(key string, value any) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.variables[key] = value
+}
