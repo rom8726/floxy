75d6267b2738527f6b6e04297add8862b2dba038
Roman Chudov <roman.chudov@gmail.com>
2025-10-26 23:59:06 +0300

    Makefiles updated.

--- Makefile
+++ Makefile
@@ -16,5 +16,10 @@ NOCOLOR="\033[0m"
 #
 
 .PHONY: help
-help: ## Выводит это сообщение
+help: ## Prints this message
 	@echo "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s :)"
+
+.PHONY: test
+test: ## Run unit tests
+	@go test -cover -coverprofile=coverage.out -v ./...
+	@go tool cover -html=coverage.out -o coverage.html
--- dev/dev.mk
+++ dev/dev.mk
@@ -1,12 +1,12 @@
 _COMPOSE=docker compose -f dev/docker-compose.yml --project-name ${NAMESPACE}
 
-dev-up: ## Поднять окружение в docker compose
+dev-up: ## Up the environment in docker compose
 	${_COMPOSE} --profile env up -d
 
-dev-down: ## Погасить окружение в docker compose
+dev-down: ## Down the environment in docker compose
 	${_COMPOSE} --profile env down --remove-orphans
 
-dev-clean: ## Погасить окружение в docker compose с очисткой образов
+dev-clean: ## Down the environment in docker compose with images cleanup
 	${_COMPOSE} --profile env down --remove-orphans -v --rmi all
 
-dev-restart: dev-down dev-up ## Перезапустить окружение в docker compose
+dev-restart: dev-down dev-up ## Restart the environment in docker compose
